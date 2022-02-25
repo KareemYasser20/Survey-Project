ebf5e81715e75763b39759c51e51818e5c999e30
Kareem Yasser <karimyasser339@gmail.com>
2022-02-25 14:28:13 +0200

    post Survey data to Database.

--- lib/screens/first_question.dart
+++ lib/screens/first_question.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:im_stepper/stepper.dart';
 import 'package:flutter/material.dart';
 import 'package:survey_project/screens/login_screen.dart';
 import '../Functions/images_text_function.dart';
 import '../widgets/EmojiWidget.dart';
+import 'package:http/http.dart' as http;
 
 
 class FirstQuestion extends StatefulWidget {
@@ -26,6 +28,47 @@ class _FirstQuestionState extends State<FirstQuestion> {
     surveyData.insert(0 , widget.employeeId);
     print('First List Try : $surveyData');
   }
+
+
+  insert (List<int> data) async{
+
+    var url = Uri.parse('http://192.168.1.10:8090/api/survey');
+
+    try{
+
+      var res = await http.post(url
+        ,headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, int>{
+          "Employee_ID":data[0],
+          "First_Question":data[1],
+          "Second_Question":data[2],
+          "Third_Question":data[3],
+          "Fourth_Question":data[4],
+          "Fifth_Question":data[5],
+          "Sixth_Question":data[6],
+          "Seventh_Question":data[7],
+          "Eighth_Question":data[8],
+          "Ninth_Question":data[9],
+          "Tenth_Question":data[10],
+          "Eleventh_Question":data[11],
+          "Twelfth_Question":data[12],
+          "Thirteenth_Question":data[13],
+          "Fourteenth_Question":data[14],
+          "Fifteenth_Question":data[15],
+          "Sixteenth_Question":data[16]
+        }),
+      );
+      print('res ${res.statusCode}');
+      return res;
+
+    }catch(e){
+      print('error : $e');
+    }
+
+  }
+
   int activeStep = 0;
   int upperBound = 15;
   List<int> surveyData = [];
@@ -222,11 +265,13 @@ class _FirstQuestionState extends State<FirstQuestion> {
                           } else if (activeStep == upperBound){
                             setState(() {
                                nextButtonText = 'إنهاء';
+                               print('Last Survey Data : $surveyData');
+                               insert(surveyData);
                                Navigator.push(
                                  context,
                                  MaterialPageRoute(builder: (context) => LoginScreen())
                                );
-                               print('Last Survey Data : $surveyData');
+
                             });
                           }
                           },
